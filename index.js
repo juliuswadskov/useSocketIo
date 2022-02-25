0d380ef1bea542d6e49626b6c012abf781535f87
Raag2005 <raag2005@protonmail.ch>
2022-02-25 22:17:36 +0100

    Final commit (not tested)

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import socketIOClient from "socket.io-client";
-const ENDPOINT = "http://127.0.0.1:4001";
 
 export default function useSocketIo(endpoint, onFunctionName) {
     const [response, setResponse] = useState("");
